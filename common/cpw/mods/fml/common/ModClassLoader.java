00ddb951aa84da2eaadd5d8e0062648c180b9445
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2020-08-30 15:06:18 +1000

    Make the ClassLoader a bit simpler
    This is just CPW's code. Incompatible with Java 9+, but it was that way already.

--- common/cpw/mods/fml/common/ModClassLoader.java
+++ common/cpw/mods/fml/common/ModClassLoader.java
@@ -40,26 +40,19 @@ public class ModClassLoader extends URLClassLoader
     }
     public void addFile(File modFile) throws MalformedURLException
     {
-        ClassLoader cl=getParent();
-        if (cl instanceof URLClassLoader) {
-            URLClassLoader ucl=(URLClassLoader) cl;
             URL url = modFile.toURI().toURL();
             try {
-                Method addUrl=URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            	java.lang.reflect.Method addUrl = java.net.URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{java.net.URL.class});
                 addUrl.setAccessible(true);
-                addUrl.invoke(ucl, url);
+                addUrl.invoke(Thread.currentThread().getContextClassLoader(), new Object[]{url});
             } catch (Exception e) {
                 Loader.log.severe("A fatal error occured attempting to load a file into the classloader");
                 throw new LoaderException(e);
             }
-        }
     }
     
     public File[] getParentSources() {
-        ClassLoader cl=getParent();
-        if (cl instanceof URLClassLoader) {
-            URLClassLoader ucl=(URLClassLoader) cl;
-            URL[] pUrl=ucl.getURLs();
+            URL[] pUrl=super.getURLs();
             File[] sources=new File[pUrl.length];
             try
             {
@@ -72,7 +65,6 @@ public class ModClassLoader extends URLClassLoader
             {
                 Loader.log.throwing("ModClassLoader", "getParentSources", e);
             }
-        }
         Loader.log.severe("Unable to process our input to locate the minecraft code");
         throw new LoaderException();
     }
